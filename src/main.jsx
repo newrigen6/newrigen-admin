f74f748018fc29e02957a7d7bab55345378a0690
newrigen6 <newrigen6@gmail.com>
2026-05-20 21:23:04 +0200

    Intégration Sentry : monitoring erreurs en temps réel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import AdminApp from './AdminApp'
 import './index.css'
+
+Sentry.init({
+  dsn: 'https://071bdf9408528d4ac90794cb1cc37366@o4511423763513344.ingest.de.sentry.io/4511423846809680',
+  environment: import.meta.env.MODE,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 0.2,
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 1,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
