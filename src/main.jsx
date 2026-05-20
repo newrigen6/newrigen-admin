9a73bc5cab9cf4f5b40753338aa0a9c18b1aba75
newrigen6 <newrigen6@gmail.com>
2026-05-20 21:48:34 +0200

    Intégration PostHog : analytics utilisateurs

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import * as Sentry from '@sentry/react'
+import posthog from 'posthog-js'
 import AdminApp from './AdminApp'
 import './index.css'
+
+posthog.init('phc_uE3P2LtSmtTWwr64q7WpGZfu5M34aN2x8BBZAGiY2tD7', {
+  api_host: 'https://eu.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 Sentry.init({
   dsn: 'https://2ce32e93752527cddc7b011101bcab36@o4511423763513344.ingest.de.sentry.io/4511423862407253',
